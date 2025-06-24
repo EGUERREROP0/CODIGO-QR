0a2c737b6c8fdd45abbb9a510d4d3489f2800287
PROGRAMMINGEGP <guerreroerminio30@gamil.com>
2025-06-24 15:15:11 -0500

    f

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
 function scrollToSection(id) {
     document.getElementById(id).scrollIntoView({ behavior: 'smooth' });
 }
+// document.getElementById('shareButton').addEventListener('click', async () => {
+//     if (navigator.share) {
+//         try {
+//             await navigator.share({
+//                 title: 'Perfil de Erminio Guerrero',
+//                 text: 'Conoce el perfil profesional de Erminio Guerrero.',
+//                 url: window.location.href
+//             });
+//         } catch (error) {
+//             console.error('Error al compartir:', error);
+//         }
+//     } else {
+//         alert('La función de compartir no es compatible con tu navegador.');
+//     }
+// });
   
